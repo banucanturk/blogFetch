f374c6469a3609ae1798940bb65a1d13db564ae9
banucanturk <118526042+banucanturk@users.noreply.github.com>
2023-08-24 00:01:20 +0300

    fetch yapıldı, background degiştirildi

---
+++ js/customs.js
@@ -0,0 +1,68 @@
+const postsContainer = document.querySelector('#postsContainer');
+const baseUrl = 'http://localhost:1337/api/';
+const postTitle = document.querySelector('#postTitle');
+const postSlug = document.querySelector('#postSlug');
+
+
+async function getPosts() {
+    let posts = await fetch('http://localhost:1337/api/posts?populate=*')
+        .then(response => response.json());
+    loadPage(posts.data);
+}
+
+function loadPage(posts) {
+    posts.forEach(post => {
+        postsContainer.innerHTML += `<div class="post-preview">
+        <a href="#/posts/${post.id}">
+            <h2 class="post-title">${post.attributes.title}</h2>
+            <h3 class="post-subtitle">${post.attributes.content}</h3>
+        </a>
+        <p class="post-meta">
+            Posted by
+            <a href="#!">${post.attributes.user.data.attributes.username}</a>
+            on ${post.attributes.updatedAt.substr(0, 10)}
+        </p>
+    </div>
+    <!-- Divider-->
+    <hr class="my-4" />`;
+
+    });
+
+    window.addEventListener('hashchange', () => {
+        let url = location.hash.substr(2);
+        if (url === '') {
+            loadPage(posts)
+        } else {
+            fetch(baseUrl + url)
+                .then(response => response.json())
+                .then(result => {
+                    postsContainer.innerHTML = `
+                    <article class="mb-4">
+                    <div class="container px-4 px-lg-5">
+                        <div class="row gx-4 gx-lg-5 justify-content-center">
+                            <div class="col-md-10 col-lg-8 col-xl-7">
+                                ${result.data.attributes.content}
+                            </div>
+                        </div>
+                    </div>
+                </article>`;
+
+                    postTitle.textContent = result.data.attributes.title
+                    postSlug.textContent = result.data.attributes.slug
+                })
+        }
+    });
+
+    postsContainer.querySelectorAll('.post-preview').forEach((postElement, index) => {
+        postElement.addEventListener('click', () => {
+            const backgroundURL = postElement.getAttribute('data-background');
+
+            // Header'ın arka planını değiştirdim
+            const header = document.querySelector('.masthead');
+            header.style.backgroundImage = `url('${'assets/img/images.png'}')`;
+
+        });
+    });
+}
+
+getPosts();
---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+/*!
+* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
+*/
+const urlPrefix = 'https://jsonplaceholder.org/';
+const requestPostsUrl = urlPrefix + 'posts';
+const btn = document.querySelector('.olderPost');
+let posts = [];
+
+
+
+btn.addEventListener('click', function () {
+    fetch(requestPostsUrl)
+        .then(response => response.json())
+        .then(json => {
+            posts = json;
+            render();
+        });
+})
+
+function renderPosts() {
+    for (const post of posts) {
+        console.log(post.id);
+
+    }
+
+}
+
+function getFirstSentence(content) {
+    const sentences = content.split('.');
+    if (sentences.length > 0) {
+        return sentences[0] + '.';
+    }
+    return content;
+}
+
+
+const colmd10 = document.querySelector('.posts .row .col-md-10');
+
+
+function render() {
+    for (let i = 0; i < 5; i++) {
+        const currPost = posts[i];
+        const firstSentence = getFirstSentence(currPost.content);
+        colmd10.innerHTML += `
+            <div class="post-preview">
+                <a href="post.html">
+                    <h2 class="post-title">${currPost.title}</h2>
+                    <h3 class="post-subtitle">${firstSentence}</h3>
+                </a>
+                <p class="post-meta">
+                    " Posted by "
+                    <a href="#!">Start Bootstrap</a>
+                    " on ${currPost.publishedAt}"
+                </p>
+            </div>
+            <hr>
+        `;
+    }
+    bindPostsClicks();
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function () {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if (currentTop < scrollPos) {
+            // Scrolling Up
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+            // Scrolling Down
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+})
